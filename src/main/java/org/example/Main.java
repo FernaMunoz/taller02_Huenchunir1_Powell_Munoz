06c2688b0b860a55fe418232e1fbe191d47c604d
mjosepc <mjose.powell@gmail.com>
2023-09-28 10:27:59 -0300

    3 commit el menu María jose

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 import java.util.Scanner;
 import java.util.*;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     private static final Map<String, List<String>> habitaciones = new HashMap<>();
 
@@ -19,85 +17,87 @@ public class Main {
                 case 1:
                     System.out.println("Reservar una habitacion");
                     menuReserva(sc);
-
+                    break;
                 case 2:
                     System.out.println("Imprimir boleta");
                     //imprimirBoleta();
-                    break:
+                    break;
                 case 3:
                     System.out.println("Limpiar todas las habitaciones");
-                    //resetAll();
-                    break:
-
+                    resetAll();
+                    break;
                 case 4:
-                    System.out.println("saliendo del menu");
+                    System.out.println("Saliendo del menu");
                     break;
                 default:
-                    System.out.println("opcion no valida");
+                    System.out.println("Opción no válida");
+            }
+        } while (opcion != 4);
 
-            } while (opcion != 4) ;
-
-            sc.close();
-        }
+        sc.close();
     }
 
     public static void hotel() {
         for (int i = 1; i <= 10; i++) {
-            habitaciones.put("Habitación " + i, Arrays.asList("disponible", "S.E", "S.E"));//S.E, es sin especificar el cual dejamos por defecto
-            System.out.println(habitaciones);
+            habitaciones.put("Habitación " + i, Arrays.asList("disponible", "S.E", "S.E")); // S.E, es sin especificar el cual dejamos por defecto
         }
     }
 
-
-  /*  public static void cambiarEstadoHabitacion(int habitacion) {
-
-
+    public static void reservar() {
+        List<String> habitacionesDisponibles = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : habitaciones.entrySet()) {
+            if ("disponible".equals(entry.getValue().get(0))) {
+                habitacionesDisponibles.add(entry.getKey());
+            }
+        }
+        System.out.println("Habitaciones disponibles: " + habitacionesDisponibles);
     }
 
-    public static void imprimirBoleta() {
-        System.out.println("hola");
-        int habitacion = sc.nextInt();
-        if (habitaciones.containsKey(habitacion)) {
-
+    public static void resetAll() {
+        for (Map.Entry<String, List<String>> entry : habitaciones.entrySet()) {
+            entry.setValue(Arrays.asList("disponible", "S.E", "S.E"));
         }
-
-
-    }*/
+        System.out.println("Todas las habitaciones han sido restablecidas a 'disponible'");
+    }
 
     public static void Menu() {
         System.out.println("MENU");
-        System.out.println("1, Reservar");
-        System.out.println("2, Imprimir");
-        System.out.println("3, ResetAll");
-        System.out.println("4, Salir");
-        System.out.println("Seleccione la opcion que desea realizar");
+        System.out.println("1. Reservar");
+        System.out.println("2. Imprimir");
+        System.out.println("3. ResetAll");
+        System.out.println("4. Salir");
+        System.out.println("Seleccione la opción que desea realizar");
     }
 
     public static void menuReserva(Scanner sc) {
-        System.out.println("Menu para las reservas");
-        System.out.println("1, Reservar");
-        System.out.println("2, confirmar reserva");
-        System.out.println("3, salir");
-        System.out.println("Seleccione la opcion que desea realizar");
-        int opcion = sc.nextInt();
+        int opcion;
+        do {
+            System.out.println("Menú para las reservas");
+            System.out.println("1. Reservar");
+            System.out.println("2. Confirmar reserva");
+            System.out.println("3. Salir del menú de reserva");
+            System.out.println("Seleccione la opción que desea realizar");
+            opcion = sc.nextInt();
 
-        switch (opcion) {
-            case 1:
-                //reserva();
-                //cantidadDias();
-                //servicioComida();
-                break;
-            case 2:
-                //confirmarReserva
-                //cambiarEstadoHabitacion
-                break;
-            case 3:
-                System.out.println("saliendo del menu de reserva");
-                break;
-            default:
-                System.out.println("opcion no valida");
-    } while (opcion !=3);
-
+            switch (opcion) {
+                case 1:
+                    reservar();
+                    // cantidadDias();
+                    // servicioComida();
+                    break;
+                case 2:
+                    // confirmarReserva
+                    // cambiarEstadoHabitacion
+                    break;
+                case 3:
+                    System.out.println("Saliendo del menú de reserva");
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+            }
+        } while (opcion != 3);
     }
 }
+
+
 
