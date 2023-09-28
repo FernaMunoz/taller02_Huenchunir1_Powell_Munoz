0eb8f251d273482d09c4ce7da515e1809f82d118
HP <f.munoz40@ufromail.cl>
2023-09-28 09:35:37 -0300

    primer commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,17 @@
 package org.example;
-
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[]args){
-        System.out.println("hola");
+    private Scanner scanner;
+    public void servicioComida(int numhabitacion){
+        System.out.print("¿Desea agregar servicio de alimentacion en la habitacion, ingrese S/N");
+        //
+        String respuesta = scanner.nextLine();
+        if (respuesta.equals("S")){
+            habitaciones[numhabitacion - 1] = "OA";
+            System.out.print("Habitacion con comida asignada");
+        } else {
+            habitaciones[numhabitacion -1] = "OS";
+        }
     }
 }
