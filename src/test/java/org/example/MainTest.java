10d2c9b9382c224b98479fc5fb75b088d66a66c2
mjosepc <mjose.powell@gmail.com>
2023-09-28 10:58:55 -0300

    pruebas

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void reservar() {
+    }
+
+    @Test
+    void cantidadDias() {
+    }
+
+    @Test
+    void servicioComida() {
+    }
+
+    @Test
+    void confirmarReserva() {
+    }
+
+    @Test
+    void imprimirBoleta() {
+    }
+
+    @Test
+    void resetAll() {
+    }
+}
